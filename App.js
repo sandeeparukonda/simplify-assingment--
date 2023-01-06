5a74fdb8ae49c9d6bcce08ba199466e255924468
Sandeeparukonda <113174887+sandeeparukonda@users.noreply.github.com>
2023-01-06 12:03:30 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="bg-container">
+      <div className="box-card">
+        <h2 className="title">
+          HTML <span>&</span> CSS
+        </h2>
+        <p>design and build</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="bg-container">
+      <div className="box-card">
+        <h2 className="title">
+          HTML <span>&</span> CSS
+        </h2>
+      </div>
+    </div>
+  );
+}
